2d6078ee1f25efa07ea3874d5747a56f067d9226
Adam Scanga <59103444+sandmanscanga@users.noreply.github.com>
2023-06-09 05:14:15 -0400

    Added a placeholder Python file to ensure that the workflows do not fail.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Module containing generic placeholder code for the project."""
+
+
+def main() -> None:
+    """Execute the main process."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
